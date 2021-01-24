6f466a5de52f3c019d18744f11d2940c5b491816
Jonathan Yong <yongrenjie@gmail.com>
2021-01-24 02:05:15 +0000

    Some progress on the list function

--- src/Abbot/Commands/List.hs
+++ src/Abbot/Commands/List.hs
@@ -4,14 +4,18 @@ module Abbot.Commands.List
 
 import           Abbot.Commands.Shared
 import           Abbot.Reference
+import           Abbot.Style                    ( setBold )
 
 import           Data.IntMap                    ( IntMap )
 import qualified Data.IntMap                   as IM
 import qualified Data.IntSet                   as IS
+import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as TIO
 import           Lens.Micro.Platform
 import           Text.Megaparsec
+import           System.Console.Terminal.Size  as TS
+
 
 runList :: ReplArgs -> IntMap Reference -> CmdOutput
 runList args refs = if IM.null refs
@@ -20,28 +24,94 @@ runList args refs = if IM.null refs
     let parsedArgs = parse pRefnos "" args
         numRefs    = fst $ IM.findMax refs
     in  case parsedArgs of
-          Left  bundle -> cmdErrS ("list: " ++ errorBundlePretty bundle)  -- parse error
-          Right refnos -> if IS.null refnos
-            then do
-              prettyPrintRefHead
-              mapM_ (prettyPrintRef refs) [1 .. numRefs]
-              pure $ Right (refs, Just $ IS.fromList [1 .. numRefs])
-            else case IS.lookupGT numRefs refnos <|> IS.lookupLT 1 refnos of
+          Left bundle -> cmdErrS ("list: " ++ errorBundlePretty bundle)  -- parse error
+          Right refnos ->
+            -- Some refnos were specified. First, check for out of bounds.
+            case IS.lookupGT numRefs refnos <|> IS.lookupLT 1 refnos of
               Just x ->
                 cmdErrS ("list: reference " <> show x <> " is out of bounds")
+              -- No out of bounds. If refnos is empty, then print all references, otherwise
+              -- print whatever was specified.
               Nothing -> do
-                prettyPrintRefHead
-                mapM_ (prettyPrintRef refs) (IS.toList refnos)
-                pure $ Right (refs, Just refnos)
+                let
+                  refnosToPrint = if IS.null refnos
+                    then IS.fromList [1 .. numRefs]
+                    else refnos
+                result <- prettyFormatRefs (IM.restrictKeys refs refnosToPrint)
+                case result of
+                  Left  errMsg        -> cmdErr errMsg
+                  Right formattedRefs -> do
+                    TIO.putStrLn formattedRefs
+                    pure $ Right (refs, Just refnos)
 
--- | Pretty-print the header of the reference list.
-prettyPrintRefHead :: IO ()
-prettyPrintRefHead = putStrLn "REFERENCES\n=========="
 
--- | Pretty-print a specific reference from the reference list, given a
--- (1-indexed) number. The output is (well, should be) based on the terminal
--- width.
-prettyPrintRef :: IntMap Reference -> Int -> IO ()
-prettyPrintRef refs index = do
-  TIO.putStr (T.pack (show index) <> ": ")
-  TIO.putStrLn (refs ^. ix index . title)
+-- | The field sizes for pretty-printing. Note that the title field is also
+-- responsible for the DOI, as well as the availability columns.
+-- Note that the first four field sizes include the padding.
+data FieldSizes = FieldSizes { numberF  :: Int
+                             , authorF  :: Int
+                             , yearF    :: Int
+                             , journalF :: Int
+                             , titleF   :: Int
+                             }
+
+-- | The amount of columns acting as padding between adjacent fields.
+fieldPadding :: Int
+fieldPadding = 2
+
+-- | Count the total of field sizes.
+totalSizes :: FieldSizes -> Int
+totalSizes fss = sum $ map ($ fss) [numberF, authorF, yearF, journalF, titleF]
+
+-- | Calculate the correct field sizes based on the terminal size.
+getFieldSizes :: IntMap Reference -> IO FieldSizes
+getFieldSizes refs = do
+  -- Number field.
+  let maxRef    = fst $ IM.findMax refs
+      numberF'  = length (show maxRef) + fieldPadding
+  -- TODO Author field.
+  let authorF'  = 7 + fieldPadding   -- NOT CORRECT
+  -- Year field.
+  let yearF'    = 4 + fieldPadding
+  -- Journal field.
+  let journalF' = fieldPadding + maximum (map (T.length . getShortestJournalName) (IM.elems refs))
+  -- Title field. The lower limit of 40 is to make sure that ANSI escape sequences
+  -- in the availability row (printed under the title) don't get messed up.
+  Just (Window _ (ncols :: Int)) <- TS.size  -- errors if output is not to a terminal
+  let titleF' = max 40 (ncols - numberF' - authorF' - yearF' - journalF')
+  pure $ FieldSizes numberF' authorF' yearF' journalF' titleF'
+
+-- | Prettify an IntMap of references.
+prettyFormatRefs :: IntMap Reference -> IO (Either Text Text)
+prettyFormatRefs refs = if IM.null refs
+  then pure $ Left "no references found"
+  else do
+    fss <- getFieldSizes refs
+    let text = prettyFormatHead fss
+          <> T.intercalate "\n\n" (map (printRef fss) (IM.assocs refs))
+    pure $ Right text
+
+-- | Generate a pretty header for the reference list.
+prettyFormatHead :: FieldSizes -> Text
+prettyFormatHead fss =
+  setBold
+      (mconcat
+        [ T.justifyLeft (numberF fss) ' ' "#"
+        , T.justifyLeft (authorF fss) ' ' "Authors"
+        , T.justifyLeft (yearF fss) ' ' "Year"
+        , T.justifyLeft (journalF fss) ' ' "Journal"
+        , T.justifyLeft (titleF fss) ' ' "Title and DOI"
+        ]
+      )
+    <> "\n"
+    <> T.replicate (totalSizes fss) "-"
+
+-- | Generate pretty output for one particular reference in an IntMap.
+printRef :: FieldSizes -> (Int, Reference) -> Text
+printRef fss (index, ref) = mconcat
+  [ T.justifyLeft (numberF fss)  ' ' (T.pack $ show index)
+  , T.justifyLeft (authorF fss)  ' ' ".author."
+  , T.justifyLeft (yearF fss)    ' ' (T.pack . show $ ref ^. year)
+  , T.justifyLeft (journalF fss) ' ' (getShortestJournalName ref)
+  , T.justifyLeft (titleF fss)   ' ' (ref ^. title)
+  ]
--- src/Abbot/Reference.hs
+++ src/Abbot/Reference.hs
@@ -6,7 +6,11 @@ module Abbot.Reference
   ) where
 
 import           Data.Aeson
+import           Data.Char                      ( isAlphaNum
+                                                , isSpace
+                                                )
 import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
 import           Data.Time.Clock
 import           GHC.Generics
 import           Lens.Micro.Platform
@@ -36,9 +40,16 @@ data Author = Author
 makeLenses ''Reference
 makeLenses ''Author
 
+-- | Instances which allow references to be serialised as YAML.
 instance ToJSON Reference where
   toEncoding = genericToEncoding defaultOptions
 instance FromJSON Reference
 instance ToJSON Author where
   toEncoding = genericToEncoding defaultOptions
 instance FromJSON Author
+
+-- | Produce as short a journal name as possible, by removing special characters
+-- (only alphanumeric characters and spaces are retained), as well as using some
+-- acronyms such as "NMR".
+getShortestJournalName :: Reference -> Text
+getShortestJournalName ref = T.strip $ T.filter ((||) <$> isAlphaNum <*> isSpace) (ref ^. journalShort)
