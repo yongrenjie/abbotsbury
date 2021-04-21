4d0c48298c09089a6b92b06e5fdabc1594b1db4e
Jonathan Yong <yongrenjie@gmail.com>
2021-04-21 23:51:35 +0100

    Add various author formatting methods

--- src/Abbot/Reference.hs
+++ src/Abbot/Reference.hs
@@ -49,15 +49,39 @@ instance FromJSON Author
 
 
 -- | Methods of formatting author names.
-data AuthorFormatting = ListCmd  -- For the list command.
+data AuthorFormatting = ListCmd          -- For the list command.
+                      | FamilyInitials   -- ACS style.
+                      | InitialsFamily   -- ACIE style.
+                      | BibLaTeX         -- For .bib files.
                       deriving (Ord, Eq, Show)
 
 -- | Formats an Author according to the specified AuthorFormatting mode.
 formatAuthor :: AuthorFormatting -> Author -> Text
-formatAuthor fmt auth = case _given auth of
-  Nothing  -> _family auth
-  Just gvn -> case fmt of
-    ListCmd ->
-      T.pack (map T.head (T.split (\c -> isSpace c || c == '-') gvn))
-        <> " "
-        <> _family auth
+formatAuthor fmt auth =
+  let fam = _family auth
+  in
+    case _given auth of
+      Nothing  -> fam
+      Just gvn -> case fmt of
+        ListCmd ->
+          T.pack (map T.head (T.split (\c -> isSpace c || c == '-') gvn))
+            <> " "
+            <> fam
+        FamilyInitials -> fam <> ", " <> makeInitials gvn
+        InitialsFamily -> makeInitials gvn <> " " <> fam
+        BibLaTeX -> T.replace ". " ".\\ " (fam <> ", " <> gvn)
+
+
+-- | Extracts the initials from a given name.
+-- 
+-- >>> makeInitials "Jonathan Ren Jie"
+-- "J. R. J."
+-- >>> makeInitials "Jean-Baptiste Simon"
+-- "J.-B. S."
+makeInitials :: Text -> Text
+makeInitials name0 = name4
+  where
+    name1 = map (T.split (== '-')) (T.split isSpace name0)  -- [["Jean", "Baptiste"], ["Simon"]]
+    name2 = (fmap . fmap) ((`T.cons` ".") . T.head) name1   -- [["J.", "B."], ["S."]]
+    name3 = map (T.intercalate "-") name2                   -- ["J.-B.", "S."]
+    name4 = T.intercalate " " name3                         -- "J.-B. S."
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,15 +1,66 @@
 import           Abbot.Reference
 
+import           Data.Map                       ( Map )
+import qualified Data.Map                      as M
 import           Test.Tasty
 import           Test.Tasty.HUnit
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
 
 
-test1, test2 :: TestTree
-test1 = testCase "one plus one is two?" (1 + 1 @?= 2)
-test2 = testCase "two times two is five?" (2 * 2 @?= 4)
+-- Define all possible inputs
+authJon, authTim, authAli, authJB :: Author
+authJon = Author (Just "Jonathan Ren Jie") "Yong"
+authTim = Author (Just "Tim D. W.") "Claridge"
+authAli = Author (Just "Mohammadali") "Foroozandeh"
+authJB = Author (Just "Jean-Baptiste") "Verstraete"   -- useful for hyphens. Thanks JB
+
+-- Define all possible (input, output) pairs
+expectedOutputsListCmd :: [(Author, Text)]
+expectedOutputsListCmd =
+  [ (authJon, "JRJ Yong"),
+    (authTim, "TDW Claridge"),
+    (authAli, "M Foroozandeh"),
+    (authJB, "JB Verstraete")
+  ]
+
+expectedOutputsFamilyInitials :: [(Author, Text)]
+expectedOutputsFamilyInitials =
+  [ (authJon, "Yong, J. R. J."),
+    (authTim, "Claridge, T. D. W."),
+    (authAli, "Foroozandeh, M."),
+    (authJB, "Verstraete, J.-B.")
+  ]
+
+expectedOutputsInitialsFamily :: [(Author, Text)]
+expectedOutputsInitialsFamily =
+  [ (authJon, "J. R. J. Yong"),
+    (authTim, "T. D. W. Claridge"),
+    (authAli, "M. Foroozandeh"),
+    (authJB, "J.-B. Verstraete")
+  ]
+
+expectedOutputsBibLaTeX :: [(Author, Text)]
+expectedOutputsBibLaTeX =
+  [ (authJon, "Yong, Jonathan Ren Jie"),
+    (authTim, "Claridge, Tim D.\\ W."),
+    (authAli, "Foroozandeh, Mohammadali"),
+    (authJB, "Verstraete, Jean-Baptiste")
+  ]
+
+-- Helper function to make tests from the (input, output) pairs
+mkTestCase :: AuthorFormatting -> (Author, Text) -> TestTree
+mkTestCase fmt (auth, expected)
+  = testCase (T.unpack (_family auth)) (formatAuthor fmt auth @?= expected)
+
 
 tests :: TestTree
-tests = testGroup "arithmetic" [test1, test2]
+tests = testGroup "Abbot.Reference.formatAuthor"
+  [ testGroup "ListCmd" $ map (mkTestCase ListCmd) expectedOutputsListCmd
+  , testGroup "FamilyInitials" $ map (mkTestCase FamilyInitials) expectedOutputsFamilyInitials
+  , testGroup "InitialsFamily" $ map (mkTestCase InitialsFamily) expectedOutputsInitialsFamily
+  , testGroup "BibLaTeX" $ map (mkTestCase BibLaTeX) expectedOutputsBibLaTeX
+  ]
 
 main :: IO ()
 main = defaultMain tests
