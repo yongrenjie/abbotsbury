32294cae16e3d5c24e3ddc86c8964fbd9b4c543b
Jonathan Yong <yongrenjie@gmail.com>
2021-05-11 00:24:32 +0100

    app: implement pipes for cite

--- app/Commands/Cite.hs
+++ app/Commands/Cite.hs
@@ -14,11 +14,12 @@ import           Data.IntSet                    ( IntSet )
 import qualified Data.IntSet                   as IS
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as M
+import           Data.Maybe                     ( isNothing )
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as TIO
-import           System.Process
 import           Lens.Micro.Platform
+import           System.Process
 import           Text.Megaparsec
 
 
@@ -50,20 +51,26 @@ runCite args input = do
         (throwErrorWithPrefix
           ("reference(s) " <> intercalateCommas badRefnos <> " not found")
         )
-      -- Then, check if refnos is empty
-      when (IS.null refnos) (throwError "cite: no references selected")
+      -- Figure out which refnos to print
+      refsToCite <- case (varin input, IS.null refnos) of
+        (Nothing , True ) -> throwErrorWithPrefix "no references selected"
+        (Nothing , False) -> pure $ IM.elems (refs `IM.restrictKeys` refnos)
+        (Just set, True ) -> pure $ IM.elems (refs `IM.restrictKeys` set)
+        (Just set, False) ->
+          throwErrorWithPrefix "cannot specify refnos and a pipe simultaneously"
       -- Generate the citation(s)
-      let (style, format) = getStyleFormat rules
-          refsToCite = IM.elems (IM.restrictKeys refs refnos)
-          citations = T.intercalate "\n\n" $ map (cite style format . _work) refsToCite
+      let
+        (style, format) = getStyleFormat rules
+        citations =
+          T.intercalate "\n\n" $ map (cite style format . _work) refsToCite
       -- Print them
       liftIO $ TIO.putStrLn citations
       -- Copy them
       liftIO $ case rules of
-                    AcsWord -> do
-                      let htmlLines = map (cite style htmlFormat . _work) refsToCite
-                      copyHtmlLinesAsRtf htmlLines
-                    _ -> copy citations
+        AcsWord -> do
+          let htmlLines = map (cite style htmlFormat . _work) refsToCite
+          copyHtmlLinesAsRtf htmlLines
+        _ -> copy citations
       -- Return basically nothing
       pure $ SCmdOutput refs Nothing
 
