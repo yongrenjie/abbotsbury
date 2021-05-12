ada9a9db57ffa1e72ad9c7c13b8e3c6824a6b9cb
Jonathan Yong <yongrenjie@gmail.com>
2021-05-12 22:48:05 +0100

    tests: add BibLaTeX check on OL paper

--- tests/Cite.hs
+++ tests/Cite.hs
@@ -3,39 +3,56 @@ module Cite
   ) where
 
 import           Abbotsbury
+import qualified Data.Text                     as T
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import qualified TestWorks                     as TW
 
--- | This is the only test for which we will exhaustively check all formats. For other tests we can
--- just check Markdown alone.
+-- | This is the only test for which we will exhaustively check all formats. For
+-- other tests we can just check Markdown alone.
 testOLCite :: TestTree
-testOLCite = testGroup "cite - 2019 OL - ACS style"
-                       [text, markdown, restructured, html]
+testOLCite = testGroup "cite - 2019 OL - all styles"
+                       [acsText, acsMarkdown, acsRestructured, acsHtml, bib]
  where
-  text :: TestTree
-  text       = testCase "text format" $ textActual @?= textExpected
+  acsText :: TestTree
+  acsText    = testCase "ACS/Text" $ textActual @?= textExpected
   textActual = cite acsStyle textFormat TW.orgLett
   textExpected
     = "Mansfield, S. J.; Smith, R. C.; Yong, J. R. J.; Garry, O. L.; Anderson, E. A. A General Copper-Catalyzed Synthesis of Ynamides from 1,2-Dichloroenamides. Org. Lett. 2019, 21 (8), 2918-2922. DOI: 10.1021/acs.orglett.9b00971."
-  markdown :: TestTree
-  markdown = testCase "Markdown format" $ markdownActual @?= markdownExpected
+  acsMarkdown :: TestTree
+  acsMarkdown =
+    testCase "ACS/Markdown" $ markdownActual @?= markdownExpected
   markdownActual = cite acsStyle markdownFormat TW.orgLett
   markdownExpected
     = "Mansfield, S. J.; Smith, R. C.; Yong, J. R. J.; Garry, O. L.; Anderson, E. A. A General Copper-Catalyzed Synthesis of Ynamides from 1,2-Dichloroenamides. *Org. Lett.* **2019,** *21* (8), 2918-2922. DOI: [10.1021/acs.orglett.9b00971](https://doi.org/10.1021/acs.orglett.9b00971)."
-  restructured :: TestTree
-  restructured =
-    testCase "ReStructuredText format"
+  acsRestructured :: TestTree
+  acsRestructured =
+    testCase "ACS/reStructuredText"
       $   restructuredActual
       @?= restructuredExpected
   restructuredActual = cite acsStyle restructuredFormat TW.orgLett
   restructuredExpected
     = "Mansfield, S. J.; Smith, R. C.; Yong, J. R. J.; Garry, O. L.; Anderson, E. A. A General Copper-Catalyzed Synthesis of Ynamides from 1,2-Dichloroenamides. *Org. Lett.* **2019,** *21* (8), 2918-2922. DOI: `10.1021/acs.orglett.9b00971 <https://doi.org/10.1021/acs.orglett.9b00971>`_."
-  html :: TestTree
-  html       = testCase "HTML format" $ htmlActual @?= htmlExpected
+  acsHtml :: TestTree
+  acsHtml    = testCase "ACS/HTML" $ htmlActual @?= htmlExpected
   htmlActual = cite acsStyle htmlFormat TW.orgLett
   htmlExpected
     = "Mansfield, S. J.; Smith, R. C.; Yong, J. R. J.; Garry, O. L.; Anderson, E. A. A General Copper-Catalyzed Synthesis of Ynamides from 1,2-Dichloroenamides. <i>Org. Lett.</i> <b>2019,</b> <i>21</i> (8), 2918-2922. DOI: <a href=\"https://doi.org/10.1021/acs.orglett.9b00971\">10.1021/acs.orglett.9b00971</a>."
+  bib :: TestTree
+  bib = testCase "BibLaTeX" $ bibActual @?= bibExpected
+  bibActual = cite bibStyle textFormat TW.orgLett
+  bibExpected
+    = T.intercalate "\n" [ "@article{MansfieldOL2019,"
+                         , "    doi = {10.1021/acs.orglett.9b00971},"
+                         , "    author = {Mansfield, Steven J.\\ and Smith, Russell C.\\ and Yong, Jonathan R.\\ J.\\ and Garry, Olivia L.\\ and Anderson, Edward A.},"
+                         , "    journal = {Org.\\ Lett.},"
+                         , "    title = {A General Copper-Catalyzed Synthesis of Ynamides from 1,2-Dichloroenamides},"
+                         , "    year = {2019},"
+                         , "    volume = {21},"
+                         , "    number = {8},"
+                         , "    pages = {2918-2922},"
+                         , "}" ]
+
 
 tests :: TestTree
 tests = testGroup "Cite" [testOLCite]
