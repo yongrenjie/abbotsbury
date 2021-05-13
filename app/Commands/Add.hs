3551fd432584f0ea24663400f590a16740636042
Jonathan Yong <yongrenjie@gmail.com>
2021-05-13 15:00:06 +0100

    app: refactor Add code a bit

--- app/Commands/Add.hs
+++ app/Commands/Add.hs
@@ -4,7 +4,7 @@ module Commands.Add
 
 import           Abbotsbury
 import           Commands.Shared
-import qualified Control.Exception             as CE
+import           Control.Exception
 import           Data.Either                    ( partitionEithers )
 import qualified Data.IntMap                   as IM
 import           Data.Text                      ( Text )
@@ -18,8 +18,11 @@ import           Reference
 import           System.Environment
 import           System.Process
 
+prefix :: Text
+prefix = "add: "
+
 throwErrorWithPrefix :: Text -> ExceptT Text IO a
-throwErrorWithPrefix e = throwError $ "add: " <> e
+throwErrorWithPrefix e = throwError $ prefix <> e
 
 runAdd :: Args -> CmdInput -> CmdOutput
 runAdd args input = do
@@ -29,37 +32,36 @@ runAdd args input = do
   when (null dois) $ throwErrorWithPrefix "no DOIs supplied"
   -- TODO: minimal verification of DOI legitimacy, to prevent spurious requests.
   -- Try to get the email to use for Crossref.
-  maybeEmail <- liftIO getEmailForCrossref
-  case maybeEmail of
-    Nothing -> throwErrorWithPrefix
+  email <- getEmailForCrossref
+  -- Fetch the data from Crossref.
+  crossrefResponses <- liftIO $ fetchWorks email dois
+  let (exceptions, newWorks) = partitionEithers crossrefResponses
+  forM_
+    exceptions
+    (\e -> printError
+      (  "add: could not get Crossref data for DOI '"
+      <> getDoiFromException e
+      <> "'"
+      )
+    )
+  now <- liftIO getCurrentTime
+  let newRefs = map (\w -> Reference w [] now now) newWorks
+      refsout = IM.fromList $ zip [1 ..] (IM.elems refs ++ newRefs)
+  pure $ SCmdOutput refsout Nothing
+
+getEmailForCrossref :: ExceptT Text IO Text
+getEmailForCrossref = do
+  -- Try to get the environment variable.
+  maybeEnvvar <- liftIO $ lookupEnv "ABBOT_EMAIL"
+  -- Try to get it from the gitconfig.
+  let getGitEmail =
+        Just <$> readProcess "git" ["config", "--get", "user.email"] []
+  maybeGit <- liftIO $ catch getGitEmail (\(e :: IOException) -> pure Nothing)
+  case (maybeEnvvar, maybeGit) of
+    (Just e , _      ) -> pure $ T.pack e
+    (Nothing, Just e') -> pure $ T.pack e'
+    _                  -> throwErrorWithPrefix
       (  "no email was specified. "
       <> "Please set either the ABBOT_EMAIL environment variable, "
       <> "or set an email in your .gitconfig file."
       )
-    Just email -> do
-      -- Fetch the data from Crossref.
-      crossrefResponses <- liftIO $ fetchWorks email dois
-      let (exceptions, newWorks) = partitionEithers crossrefResponses
-      forM_
-        exceptions
-        (\e -> printError
-          (  "add: could not get Crossref data for DOI '"
-          <> getDoiFromException e
-          <> "'"
-          )
-        )
-      now <- liftIO getCurrentTime
-      let newRefs = map (\w -> Reference w [] now now) newWorks
-          refsout = IM.fromList $ zip [1 ..] (IM.elems refs ++ newRefs)
-      pure $ SCmdOutput refsout Nothing
-
-getEmailForCrossref :: IO (Maybe Text)
-getEmailForCrossref = do
-  maybeEnvvar <- lookupEnv "ABBOT_EMAIL"
-  (eitherGit :: Either CE.SomeException String) <- CE.try
-    $ readProcess "git" ["config", "--get", "user.email"] []
-  let email = case (maybeEnvvar, eitherGit) of
-        (Just e , _       ) -> Just e
-        (Nothing, Right e') -> Just e'
-        _                   -> Nothing
-  pure (T.pack <$> email)
