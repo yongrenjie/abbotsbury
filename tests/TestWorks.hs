51af91b484c6779c70ef50461a1d63b96ba86e81
Jonathan Yong <yongrenjie@gmail.com>
2021-05-15 01:30:36 +0100

    tests: fix tests for huge refactor

--- tests/TestWorks.hs
+++ tests/TestWorks.hs
@@ -1,33 +1,36 @@
 module TestWorks where
 
-import           Abbotsbury.Work
+import           Abbotsbury
 import qualified Data.List.NonEmpty            as NE
 import           Data.Text                      ( Text )
-
--- import qualified Data.Text                     as T
 
 auth :: (Text, Text) -> Author
 auth (gvn, fmy) = Author (Just gvn) fmy
 
 orgLett :: Work
-orgLett = Work wt t pub pubLoc a jL jS y v i p ed doi isbn aN
+orgLett = ArticleWork $ MkArticle { _articleTitle        = t
+                                  , _articleAuthors      = a
+                                  , _articleJournalLong  = jL
+                                  , _articleJournalShort = jS
+                                  , _articleYear         = y
+                                  , _articleVolume       = v
+                                  , _articleIssue        = i
+                                  , _articlePages        = p
+                                  , _articleDoi          = doi
+                                  , _articleNumber       = aN
+                                  }
  where
-  wt = JournalArticle
   t =
     "A General Copper-Catalyzed Synthesis of Ynamides from 1,2-Dichloroenamides"
-  pub    = "American Chemical Society (ACS)"
-  pubLoc = ""
-  a      = NE.fromList [mansfield, smith, yong, garry, anderson]
-  jL     = "Organic Letters"
-  jS     = "Org. Lett."
-  y      = 2019
-  v      = "21"
-  i      = "8"
-  p      = "2918-2922"
-  ed     = ""
-  doi    = "10.1021/acs.orglett.9b00971"
-  isbn   = ""
-  aN     = ""
+  a   = NE.fromList [mansfield, smith, yong, garry, anderson]
+  jL  = "Organic Letters"
+  jS  = "Org. Lett."
+  y   = 2019
+  v   = "21"
+  i   = "8"
+  p   = "2918-2922"
+  doi = "10.1021/acs.orglett.9b00971"
+  aN  = ""
   mansfield, smith, yong, garry, anderson :: Author
   mansfield = auth ("Steven J.", "Mansfield")
   smith     = auth ("Russell C.", "Smith")
@@ -36,23 +39,28 @@ orgLett = Work wt t pub pubLoc a jL jS y v i p ed doi isbn aN
   anderson  = auth ("Edward A.", "Anderson")
 
 nrmpCorrected :: Work
-nrmpCorrected = Work wt t pub pubLoc a jL jS y v i p ed doi isbn aN
+nrmpCorrected = ArticleWork $ MkArticle { _articleTitle        = t
+                                        , _articleAuthors      = a
+                                        , _articleJournalLong  = jL
+                                        , _articleJournalShort = jS
+                                        , _articleYear         = y
+                                        , _articleVolume       = v
+                                        , _articleIssue        = i
+                                        , _articlePages        = p
+                                        , _articleDoi          = doi
+                                        , _articleNumber       = aN
+                                        }
  where
-  wt     = JournalArticle
-  t      = "Parallel nuclear magnetic resonance spectroscopy"
-  pub    = "Springer Science and Business Media LLC"
-  pubLoc = ""
-  a      = NE.fromList [kupce, frydman, webb, yong, claridge]
-  jL     = "Nature Reviews Methods Primers"
-  jS     = "Nat. Rev. Methods Primers"
-  y      = 2021
-  v      = "1"
-  i      = "1"
-  p      = ""
-  ed     = ""
-  doi    = "10.1038/s43586-021-00024-3"
-  isbn   = ""
-  aN     = "27"
+  t   = "Parallel nuclear magnetic resonance spectroscopy"
+  a   = NE.fromList [kupce, frydman, webb, yong, claridge]
+  jL  = "Nature Reviews Methods Primers"
+  jS  = "Nat. Rev. Methods Primers"
+  y   = 2021
+  v   = "1"
+  i   = "1"
+  p   = ""
+  doi = "10.1038/s43586-021-00024-3"
+  aN  = "27"
   kupce, frydman, webb, yong, claridge :: Author
   kupce    = auth ("Ēriks", "Kupče")
   frydman  = auth ("Lucio", "Frydman")
@@ -61,44 +69,54 @@ nrmpCorrected = Work wt t pub pubLoc a jL jS y v i p ed doi isbn aN
   claridge = auth ("Tim D. W.", "Claridge")
 
 noFirstNameN2020 :: Work
-noFirstNameN2020 = Work wt t pub pubLoc a jL jS y v i p ed doi isbn aN
+noFirstNameN2020 = ArticleWork $ MkArticle { _articleTitle        = t
+                                           , _articleAuthors      = a
+                                           , _articleJournalLong  = jL
+                                           , _articleJournalShort = jS
+                                           , _articleYear         = y
+                                           , _articleVolume       = v
+                                           , _articleIssue        = i
+                                           , _articlePages        = p
+                                           , _articleDoi          = doi
+                                           , _articleNumber       = aN
+                                           }
  where
-  wt     = JournalArticle
-  t      = "Attention science: some people have only one name"
-  pub    = "Springer Science and Business Media LLC"
-  pubLoc = ""
-  a      = NE.fromList [sheherazade, ardiantiono]
-  jL     = "Nature"
-  jS     = "Nature"
-  y      = 2020
-  v      = ""
-  i      = ""
-  p      = ""
-  ed     = ""
-  doi    = "10.1038/d41586-020-02761-z"
-  isbn   = ""
-  aN     = ""
+  t   = "Attention science: some people have only one name"
+  a   = NE.fromList [sheherazade, ardiantiono]
+  jL  = "Nature"
+  jS  = "Nature"
+  y   = 2020
+  v   = ""
+  i   = ""
+  p   = ""
+  doi = "10.1038/d41586-020-02761-z"
+  aN  = ""
   sheherazade, ardiantiono :: Author
   sheherazade = Author Nothing "Sheherazade"
   ardiantiono = Author Nothing "Ardiantiono"
 
 glaserS1998 :: Work
-glaserS1998 = Work wt t pub pubLoc a jL jS y v i p ed doi isbn aN
+glaserS1998 = ArticleWork $ MkArticle { _articleTitle        = t
+                                      , _articleAuthors      = a
+                                      , _articleJournalLong  = jL
+                                      , _articleJournalShort = jS
+                                      , _articleYear         = y
+                                      , _articleVolume       = v
+                                      , _articleIssue        = i
+                                      , _articlePages        = p
+                                      , _articleDoi          = doi
+                                      , _articleNumber       = aN
+                                      }
  where
-  wt = JournalArticle
   t
     = "Unitary Control in Quantum Ensembles: Maximizing Signal Intensity in Coherent Spectroscopy"
-  pub    = "American Association for the Advancement of Science (AAAS)"
-  pubLoc = ""
   -- Crossref gives entirely wrong data for this. It has 7 authors.
-  a      = NE.fromList [auth ("S. J.", "Glaser")]
-  jL     = "Science"
-  jS     = "Science" -- not inside the Crossref JSON, so defaults to long name
-  y      = 1998
-  v      = "280"
-  i      = "5362"
-  p      = "421-424"
-  ed     = ""
-  doi    = "10.1126/science.280.5362.421"
-  isbn   = ""
-  aN     = ""
+  a   = NE.fromList [auth ("S. J.", "Glaser")]
+  jL  = "Science"
+  jS  = "Science" -- not inside the Crossref JSON, so defaults to long name
+  y   = 1998
+  v   = "280"
+  i   = "5362"
+  p   = "421-424"
+  doi = "10.1126/science.280.5362.421"
+  aN  = ""
