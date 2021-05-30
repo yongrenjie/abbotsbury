11f17fd616f58c533e7dbf16a57f3f7481e8cf35
Jonathan Yong <yongrenjie@gmail.com>
2021-05-30 21:30:15 +0100

    app: finish implementing the rudimentary `search`
    It's not meant to be smart, and basically follows exactly the same
    strategy as `cygnet` has, but it's served me well enough.

--- app/Commands/Search.hs
+++ app/Commands/Search.hs
@@ -3,10 +3,19 @@ module Commands.Search
   ) where
 
 import           Commands.Shared
+import           Data.Char                      ( isAscii
+                                                , isUpper
+                                                )
+import           Data.IntMap                    ( IntMap )
+import qualified Data.IntMap                   as IM
+import           Data.Maybe                     ( catMaybes )
+import qualified Data.Set                      as S
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as TIO
+import           Data.Text.Normalize
 import           Internal.Monad
+import           Internal.PrettyRef
 import           Lens.Micro.Platform
 import           Reference
 
@@ -16,13 +25,72 @@ prefix = "search: "
 runSearch :: Args -> CmdInput -> CmdOutput
 runSearch args input = do
   let refs    = refsin input
-      queries = T.words args -- Argument parsing here is trivial.
+      cwd     = cwdin input
+      queries = T.words . searchNormalise $ args
   -- If no refs present, or no search terms specified error immediately
   errorOnNoRefs prefix input
   when (null queries) $ throwError (prefix <> "no search terms given")
   -- TODO: do the search
+  let refsMatchingAllQueries = IM.filter (queries `allFoundIn`) refs
+  if not (IM.null refsMatchingAllQueries)
+    then liftIO $ do
+      let m = IM.size refsMatchingAllQueries
+      TIO.putStrLn
+        $  prefix
+        <> "found "
+        <> (T.pack . show $ m)
+        <> "references"
+        <> "matching all queries"
+      prettify cwd (IM.assocs refsMatchingAllQueries) >>= TIO.putStrLn
+    else do
+      let refsMatchingAnyQueries = IM.filter (queries `anyFoundIn`) refs
+      if not (IM.null refsMatchingAnyQueries)
+        then liftIO $ do
+          let n = IM.size refsMatchingAnyQueries
+          TIO.putStrLn
+            $  prefix
+            <> "found "
+            <> (T.pack . show $ n)
+            <> "references"
+            <> "matching all queries"
+          prettify cwd (IM.assocs refsMatchingAnyQueries) >>= TIO.putStrLn
+        else throwError $ prefix <> "no articles found"
   -- Return basically nothing
   pure $ SCmdOutput (refsin input) Nothing
 
-createHaystack :: Reference -> Text
-createHaystack ref = undefined
+-- | Makes a series of Text pieces containing reference metadata, against which
+-- the search query/queries can be matched.
+mkHaystack :: Reference -> [Text]
+mkHaystack ref = map searchNormalise $ shared ++ tags' ++ individual
+ where
+  shared = [ T.unwords . map mkFullName . getContributors $ ref, getTitle ref]
+  tags'  = S.toList (ref ^. tags)
+  individual = case ref ^. work of
+    ArticleWork a ->
+      [ a ^. journalShort
+      , a ^. journalLong
+      , T.filter isUpper $ a ^. journalShort
+      ]
+    BookWork b -> [b ^. publisher, b ^. edition]
+  -- | Generate a person's full name.
+  mkFullName :: Person -> Text
+  mkFullName p =
+    T.unwords . catMaybes $ [p ^. given, Just (p ^. family), p ^. suffix]
+
+-- | Strip diacritics and convert to 'folded case' to allow for case-insensitive
+-- comparison.
+searchNormalise :: Text -> Text
+searchNormalise = T.toCaseFold . T.filter isAscii . normalize NFD
+
+-- * The search queries passed to the following three functions MUST already
+-- have been passed through the searchNormalise function. This condition is not
+-- enforced.
+
+foundIn :: Text -> Reference -> Bool
+foundIn query ref = any (query `T.isInfixOf`) (mkHaystack ref)
+
+allFoundIn :: [Text] -> Reference -> Bool
+allFoundIn queries ref = all (`foundIn` ref) queries
+
+anyFoundIn :: [Text] -> Reference -> Bool
+anyFoundIn queries ref = any (`foundIn` ref) queries
